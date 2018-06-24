f686d3f928a36a32274f8f015f6c62c7c88661be
Kartavya kothari <kartavya.kothari24@gmail.com>
2018-06-25 00:19:33 +0530

    Predicting the results based on simple regression

--- Part 2 - Regression/Section 4 - Simple Linear Regression/simple_LR.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simple_LR.py
@@ -21,6 +21,10 @@ y = dataset.iloc[:,1].values
 from sklearn.cross_validation import train_test_split
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 1/3, random_state = 0)
 
+#Training the simple regression model
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(x_train,y_train)
+
+#Askinf the model to predict the test results
+y_pred = regressor.predict(x_test)
