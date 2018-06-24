80f86f7694428b3dcde756cbd353bf4bb8b743ff
Kartavya kothari <kartavya.kothari24@gmail.com>
2018-06-25 00:43:25 +0530

    Regressor fitting in R

---
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simple_LR.R
@@ -0,0 +1,17 @@
+dataset = read.csv("Salary_Data.csv")
+
+# Spliting dataset into test and training
+# install.packages('caTools')
+library(caTools)
+
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset,split == FALSE)
+
+#Feature scaling
+# training_set[,2:3] = scale(training_set[,2:3])
+# test_set[,2:3] = scale(test_set[,2:3])
+
+#Regressor fitting
