fad8d2d8d34e240efcb77f8c4776db363e87c604
Kartavya kothari <kartavya.kothari24@gmail.com>
2018-06-25 00:13:18 +0530

    Trained simple regression model

--- Part 2 - Regression/Section 4 - Simple Linear Regression/simple_LR.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simple_LR.py
@@ -15,14 +15,12 @@ import pandas as pd
 dataset = pd.read_csv("Salary_Data.csv")
 
 x = dataset.iloc[:,:-1].values
-y = dataset.iloc[:,3].values
+y = dataset.iloc[:,1].values
 
 #Spliting the data into training and test set
 from sklearn.cross_validation import train_test_split
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.2, random_state = 0)
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 1/3, random_state = 0)
 
-# #Feature scaling
-# from sklearn.preprocessing import StandardScaler
-# sc_x = StandardScaler()
-# x_train = sc_x.fit_transform(x_train)
-# x_test = sc_x.transform(x_test)
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train,y_train)
