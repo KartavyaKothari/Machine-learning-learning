efa325628d7e8149e57655d5c1c96e435a8bedcf
Kartavya kothari <kartavya.kothari24@gmail.com>
2018-06-25 09:46:51 +0530

    Visualised model prediction in R

--- Part 2 - Regression/Section 4 - Simple Linear Regression/simple_LR.R
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simple_LR.R
@@ -19,3 +19,17 @@ regressor = lm(formula = Salary ~ YearsExperience,
                data = training_set)
 
 y_pred = predict(regressor, newdata = test_set)
+
+#VISUALISING
+#install.packages('ggplot2')
+library(ggplot2)
+
+ggplot()+
+  geom_point(aes(x=training_set$YearsExperience,y=training_set$Salary),color='red')+
+  geom_line(aes(x = training_set$YearsExperience,y=predict(regressor, newdata = training_set)),color='blue')+
+  ggtitle("Salary vs Experience (Training set)")+
+  xlab("Experience")+
+  ylab("Salary")
+
+
+
